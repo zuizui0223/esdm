3f6de6f21e391a92a0052053a9f348fe1338b7e3
ZHANG RUIQI <rachelzhang0223@gmail.com>
2026-09-16 16:35:02 +0900

    feat: export interaction opportunity and biotic gain

--- src/esdm/interaction/__init__.py
+++ src/esdm/interaction/__init__.py
@@ -1,5 +1,13 @@
 """Generic state-resolved biotic-interaction primitives."""
 
+from .biotic_gain import biotic_information_gain
+from .opportunity import interaction_opportunity
 from .overlap import geographic_overlap, overlap_coefficient, state_overlap
 
-__all__ = ["geographic_overlap", "overlap_coefficient", "state_overlap"]
+__all__ = [
+    "biotic_information_gain",
+    "geographic_overlap",
+    "interaction_opportunity",
+    "overlap_coefficient",
+    "state_overlap",
+]
