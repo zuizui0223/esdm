9dff30b52a20c421f49f00b3482d6217965431fa
ZHANG RUIQI <rachelzhang0223@gmail.com>
2026-09-16 19:05:40 +0900

    feat: export explicit identification result types

--- src/esdm/identify/__init__.py
+++ src/esdm/identify/__init__.py
@@ -1,10 +1,18 @@
 """Identifiability diagnostics."""
 
-from .contraction import ContractionDiagnostic, contraction_diagnostic, identify_from_contraction
+from .contraction import (
+    ContractionDiagnostic,
+    IdentificationResult,
+    IdentificationStatus,
+    contraction_diagnostic,
+    identify_from_contraction,
+)
 from .sbc import SBCRankHistogram, sbc_rank_histogram
 
 __all__ = [
     "ContractionDiagnostic",
+    "IdentificationResult",
+    "IdentificationStatus",
     "contraction_diagnostic",
     "identify_from_contraction",
     "SBCRankHistogram",
