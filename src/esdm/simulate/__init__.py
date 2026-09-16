14d6c3979e90f1304045d7ae1c0c3742b4d5fc37
ZHANG RUIQI <rachelzhang0223@gmail.com>
2026-09-16 17:42:55 +0900

    feat: export simulation API

---
+++ src/esdm/simulate/__init__.py
@@ -0,0 +1,5 @@
+"""Generative and misspecified benchmark worlds."""
+
+from .in_model import GeneratedPresenceOnly, simulate_presence_only
+
+__all__ = ["GeneratedPresenceOnly", "simulate_presence_only"]
