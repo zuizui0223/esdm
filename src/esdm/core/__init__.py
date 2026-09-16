0790958de5e132aa857fbbc93bef401e1608fcd6
ZHANG RUIQI <rachelzhang0223@gmail.com>
2026-09-16 16:29:13 +0900

    feat: export core state community primitives

---
+++ src/esdm/core/__init__.py
@@ -0,0 +1,6 @@
+"""Core state-resolved community primitives."""
+
+from .community import CommunityStateDistribution
+from .state import StateAxis
+
+__all__ = ["CommunityStateDistribution", "StateAxis"]
