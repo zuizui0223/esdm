3fdd66745bc3a3daed64247414f88abe9a33d8e1
ZHANG RUIQI <rachelzhang0223@gmail.com>
2026-09-16 19:37:31 +0900

    feat: export ECDF SBC and ESS thinning diagnostics

--- src/esdm/identify/__init__.py
+++ src/esdm/identify/__init__.py
@@ -1,4 +1,4 @@
-"""Identifiability diagnostics."""
+"""Identifiability and calibration diagnostics."""
 
 from .contraction import (
     ContractionDiagnostic,
@@ -8,7 +8,14 @@ from .contraction import (
     identify_from_contraction,
 )
 from .design_rank import identify_parameter_from_design
-from .sbc import SBCRankHistogram, sbc_rank_histogram
+from .sbc import (
+    ESSThinnedDraws,
+    SBCRankHistogram,
+    SBCSimultaneousECDFResult,
+    ess_thin_draws,
+    sbc_ecdf_simultaneous_test,
+    sbc_rank_histogram,
+)
 
 __all__ = [
     "ContractionDiagnostic",
@@ -17,6 +24,10 @@ __all__ = [
     "contraction_diagnostic",
     "identify_from_contraction",
     "identify_parameter_from_design",
+    "ESSThinnedDraws",
     "SBCRankHistogram",
+    "SBCSimultaneousECDFResult",
+    "ess_thin_draws",
+    "sbc_ecdf_simultaneous_test",
     "sbc_rank_histogram",
 ]
