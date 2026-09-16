daccfb24468175878521ed1b5ec85d1dcdcf5412
ZHANG RUIQI <rachelzhang0223@gmail.com>
2026-09-16 17:41:45 +0900

    refactor: expose authorization from claims layer

---
+++ src/esdm/claims/authorization.py
@@ -0,0 +1,5 @@
+"""Canonical claims-layer access to observation authorization."""
+
+from esdm.authorization import AuthorizedObservation, ObservationRecord, authorize_observation
+
+__all__ = ["AuthorizedObservation", "ObservationRecord", "authorize_observation"]
