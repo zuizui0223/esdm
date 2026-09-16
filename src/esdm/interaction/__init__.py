0b7fcaf91ce8cbed141bfc8839531cedc7ce6755
ZHANG RUIQI <rachelzhang0223@gmail.com>
2026-09-16 16:31:36 +0900

    feat: initialize interaction API

---
+++ src/esdm/interaction/__init__.py
@@ -0,0 +1,5 @@
+"""Generic state-resolved biotic-interaction primitives."""
+
+from .overlap import geographic_overlap, overlap_coefficient, state_overlap
+
+__all__ = ["geographic_overlap", "overlap_coefficient", "state_overlap"]
