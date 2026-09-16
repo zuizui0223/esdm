c085a68a9786a816fdd1a01f868157f8a616474f
ZHANG RUIQI <rachelzhang0223@gmail.com>
2026-09-16 16:49:39 +0900

    feat: export held-out community transfer API

--- src/esdm/transfer/__init__.py
+++ src/esdm/transfer/__init__.py
@@ -1,5 +1,19 @@
 """Transfer diagnostics for ordered ecological information levels."""
 
 from .ceiling import PointTransferCeiling, TransferStepResult, point_transfer_ceiling
+from .community import (
+    CommunityGainResult,
+    HeldoutCommunityPrediction,
+    community_information_ceiling,
+    community_log_score_gain,
+)
 
-__all__ = ["PointTransferCeiling", "TransferStepResult", "point_transfer_ceiling"]
+__all__ = [
+    "PointTransferCeiling",
+    "TransferStepResult",
+    "point_transfer_ceiling",
+    "CommunityGainResult",
+    "HeldoutCommunityPrediction",
+    "community_information_ceiling",
+    "community_log_score_gain",
+]
